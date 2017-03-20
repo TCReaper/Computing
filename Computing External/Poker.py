dc0c4430e6c52456b658e8ffb8ff934df4c9e6fe
TCReaper <haoshaun@gmail.com>
2017-03-21 00:52:26 +0800

    Update: Finished Poker

--- Computing External/Poker.py
+++ Computing External/Poker.py
@@ -37,7 +37,7 @@
 
 
 
-hand = [(3,1),(2,1),(3,1),(4,2),(5,3)]
+hand = [(2,1),(3,3),(4,2),(5,4),(7,1)]
 cards = []
 suites = []
 cardFreq = {}
@@ -82,17 +82,84 @@ maxValue = max(cards)
 minValue = min(cards)
 
 def check():
-        if freqFreq[1] == 5:
-                if maxValue - minValue == 4:
-                        return "It is a straight flush!"
-                elif 14 in cards:
-                        print(cards)
-                        if cards[3] - cards [0] == 3:
-                                return "It is a straight flush!"
+        fourkind = 0
+        threekind = 0
+        twokind = 0
+        onekind = 0
+        for i in freqFreq:
+                if i == 4:
+                        fourkind = 1
+                elif i == 3:
+                        threekind = 1
+                elif i == 2:
+                        twocount = freqFreq[2]
+                        twokind = twocount
+                elif i == 1:
+                        onecount = freqFreq[1]
+                        onekind = onecount
+        if onekind == 5:
+                process = True
+                while process:
+                        if suites.count(suites[0]) == 5:
+                                if maxValue - minValue == 4:
+                                        setvalue = 9
+                                        #return "It is a straight flush!"
+                                elif 14 in cards: 
+                                        print(cards)
+                                        if cards[3] - cards [0] == 3:
+                                                setvalue = 9
+                                                #return "It is a straight flush!"
+                                else:
+                                        setvalue = 1
+                                        process = False
+                        else:
+                                if maxValue - minValue == 4:
+                                        setvalue = 5
+                                        #return "It is a straight!"
+                                elif 14 in cards: 
+                                        print(cards)
+                                        if cards[3] - cards [0] == 3:
+                                                setvalue = 5
+                                                #return "It is a straight!"
+                                else:
+                                        setvalue = 1
+                                        process = False
+                                
         elif suites.count(suites[0]) == 5:
-                return "It is a straight flush!"
+                setvalue = 6
+                #return "It is a flush!"
         
+        elif fourkind == 1:
+                setvalue = 8
+                #return "It is a 4 of a kind!"
+        
+        elif threekind == 1:
+                if twokind == 1:
+                        setvalue = 7
+                        #return "It is a full house!"
+                else:
+                        setvalue = 4
+                        #return "It is a 3 of a kind!"
+        elif twokind > 0:
+                if twokind == 2:
+                        setvalue = 3
+                        #return "It is a two pair!"
+                else:
+                        setvalue = 2
+                        #return "It is a single pair!"
 
+        else:
+                setvalue = 1
+
+        evaluated = setvalue * 1000
+        print(cards)
+        highestcard = max(cards)
+        highestcardindex = cards.index(highestcard)
+        highestcardsuite = suites[highestcardindex]
+        highestcard = highestcard * 10
+        evaluated = evaluated + highestcard + highestcardsuite
+        print(evaluated)
+        
 print(check())
 
 
