c75a57be27fabfafb3e3abfb68babfdf343155ac
TCReaper <haoshaun@gmail.com>
2018-09-26 23:56:31 +0800

    Post Prelim JC Reviews

---
+++ Computing Algorithms/Fibonacci Calc.py
@@ -0,0 +1,15 @@
+
+def fibonacci(n):
+      if n == 1 or n == 2:
+            return 1
+      else:
+            return fibonacci(n-1) + fibonacci(n-2)
+
+def stupidfibon(n):
+      if n == 1 or n == 2:
+            return 1
+      else:
+            pass # use global array for later use save cost
+            
+            
+                  
